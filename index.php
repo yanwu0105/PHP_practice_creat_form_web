03163a48f8fe4752f5f3227eba0e15892cf8cabb
yan <w30415@gmail.com>
2019-06-15 16:41:47 +0800

    index can show chinese

--- index.php
+++ index.php
@@ -17,9 +17,65 @@ $rows = $result->fetchAll();
       //echo "<td>".$i['id']."</td>";
       echo "<td>".$i['name']."</td>";
       echo "<td>".$i['age']."</td>";
-      echo "<td>".$i['sex']."</td>";
-      echo "<td>".$i['education']."</td>";
-      echo "<td>".$i['hobby']."</td>";
+      switch ($i['sex']) {
+        case 'Male':
+          echo "<td>男</td>";
+          break;
+        case 'Female':
+          echo "<td>女</td>";
+          break;
+        default:
+          echo "<td>na</td>";
+          break;
+      }
+      switch ($i['education']) {
+        case 'Elementary':
+          echo "<td>國小</td>";
+          break;
+        case 'Junior':
+          echo "<td>國中</td>";
+          break;
+        case 'senior':
+          echo "<td>高中</td>";
+          break;
+        case 'University':
+          echo "<td>大學</td>";
+          break;
+        case 'Graduate':
+          echo "<td>研究所以上</td>";
+          break;
+        default:
+          echo "<td>na</td>";
+          break;
+      }
+      $hobby_list = explode(",",$i["hobby"]);
+      $arrlength = count($hobby_list);
+      $hobby_ch = array();
+      for($x=0;$x<$arrlength;$x++) {
+        switch ($hobby_list[$x]) {
+          case 'Basketball':
+          array_push($hobby_ch, "籃球");
+          break;
+        case 'Baseball':
+          array_push($hobby_ch, "棒球");
+          break;
+        case 'Football':
+          array_push($hobby_ch, "足球");
+          break;
+        case 'Tennis':
+          array_push($hobby_ch, "網球");
+          break;
+        case 'Badminton':
+          array_push($hobby_ch, "羽毛球");
+          break;
+        case 'Pingpong':
+          array_push($hobby_ch, "乒乓球");
+          break; 
+        default:
+          break;
+        }
+      }
+      echo "<td>".implode(",",$hobby_ch)."</td>";
       echo "<td>".$i['info']."</td>";
       echo "<td><button type='button' name='id' onclick=(location.href='edit.php?id=".$i['id']."')>修改</button></td>";
       echo "<td><button type='button' name='id' onclick=(location.href='delete.php?id=".$i['id']."')>刪除</button></td>";
@@ -28,5 +84,4 @@ $rows = $result->fetchAll();
   </table>
 </body>
 </html>
-
 
